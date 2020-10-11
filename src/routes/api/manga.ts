8e5f6c969e9cb7fc7a4f469cff7024afc44d5fc5
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-10-11 18:32:20 -0500

    Wiring up puppeteer for searching/scraping manga from mangapanda
    package.json
      -updating packages
    manga.ts
      -changing this route to hit /search
      -wiring up puppeteer to do the webscraping of
      mangapanda searches

--- src/routes/api/manga.ts
+++ src/routes/api/manga.ts
@@ -14,7 +14,7 @@ const router: Router = express.Router();
 // @description Route to get manga (test)
 // @access Public
 router.get(
-  "/",
+  "/search",
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const searchRequest: any = req.query;
     const browser: Browser = await puppeteer.launch({
@@ -24,8 +24,75 @@ router.get(
     page.on("console", (msg: ConsoleMessage): void =>
       console.log("PAGE LOG:", msg.text())
     );
+    await page.goto("http://www.mangapanda.com/");
+
+    await page.type("#searchbox", searchRequest.manga);
+
+    // manually added this type to the Page interface
+    await page.waitForTimeout(3000)
+
+    const searchResult: Array<ElementHandle> = await page.$$(
+      "div.ac_results > ul > li"
+    );
+
+    const searchResultMapping: Array<Promise<any>> = searchResult.map(
+      async (result: ElementHandle): Promise<any> => {
+        const coverElement: ElementHandle | null = await result.$(
+          "img"
+        );
+
+        if (coverElement) {
+          const coverURL: JSHandle<any> = await coverElement.getProperty(
+            "src"
+          );
+
+          const titleElement: ElementHandle | null = await result.$(
+            "strong"
+          );
+
+          if (titleElement) {
+            const titleString: JSHandle<any> = await titleElement.getProperty("innerText")
+
+            const artistElement: ElementHandle | null = await result.$("i")
+
+            if (artistElement) {
+              const artistString: JSHandle<any> = await artistElement.getProperty("innerText")
+
+              return {
+                coverURL: await coverURL.jsonValue(),
+                titleString: await titleString.jsonValue(),
+                artistString: await artistString.jsonValue(),
+              };
+            }
+          }
+        } else {
+          const titleElement: ElementHandle | null = await result.$(
+            "strong"
+          );
+
+          if (titleElement) {
+            const titleString: JSHandle<any> = await titleElement.getProperty("innerText")
+
+            const artistElement: ElementHandle | null = await result.$("i")
+
+            if (artistElement) {
+              const artistString: JSHandle<any> = await artistElement.getProperty("innerText")
+
+              return {
+                titleString: await titleString.jsonValue(),
+                artistString: await artistString.jsonValue(),
+              };
+            }
+          }
+        }
+      }
+    );
+
+    const results = await Promise.all(searchResultMapping);
+
     await browser.close();
-    res.send("hello")
+
+    res.send(results)
   }
 );
 module.exports = router;
