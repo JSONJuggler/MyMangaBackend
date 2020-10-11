84ffc30bf71af6011912395181fc834e72356cf1
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-10-11 11:57:17 -0500

    Initial commit, with bare route for manga and puppeteer

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application } from "express";
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use(express.json());
+
+app.use("/api/manga", require("./routes/api/manga"));
---
+++ src/routes/api/manga.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response, NextFunction, Router } from "express";
+import puppeteer, {
+  Page,
+  Browser,
+  ConsoleMessage,
+  ElementHandle,
+  JSHandle,
+} from "puppeteer";
+
+// create router
+const router: Router = express.Router();
+
+// @route GET api/manga
+// @description Route to get manga (test)
+// @access Public
+router.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const searchRequest: any = req.query;
+    const browser: Browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page: Page = await browser.newPage();
+    page.on("console", (msg: ConsoleMessage): void =>
+      console.log("PAGE LOG:", msg.text())
+    );
+    res.send("hello")
+  }
+);
+module.exports = router;
