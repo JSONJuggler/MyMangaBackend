c32f035dc99232c74c9bd61a13b1e7d2e6c85b53
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-10-23 00:31:19 -0500

    Hacky (and probably bad/inefficient) work around for very long initial loading time. Instead of scraping/fetching every page, I am scraping the first six then the rest one by one.

--- src/routes/api/manga.ts
+++ src/routes/api/manga.ts
@@ -275,7 +275,7 @@ router.get(
 
     const chapterImageUrls = [];
 
-    for (let i = 0; i < chapterPageUrls.length; i++) {
+    for (let i = 0; i < 6; i++) {
       try {
 
         await page.goto(
@@ -328,12 +328,99 @@ router.get(
     }
 
     const result = {
+      chapterPageUrls,
       chapterImageUrls,
     }
-
+    console.log(result)
     await browser.close();
 
     res.send(result)
+  }
+);
+
+// @route GET api/manga/pages
+// @description Route to get manga pages
+// @access Public
+router.get(
+  "/page",
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+
+    const { chapter }: any = req.query;
+
+    const browser: Browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+
+    const page: Page = await browser.newPage();
+
+    await page.setUserAgent(
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+    );
+
+
+    // page.on("console", (msg: ConsoleMessage): void =>
+    //   console.log("PAGE LOG:", msg.text())
+    // );
+
+    // console.log(chapterLandingUrl)
+    try {
+      await page.goto("http://www.mangapanda.com" + chapter
+        , { waitUntil: "domcontentloaded", timeout: 3000 });
+    } catch (e) {
+
+      await page.reload({ waitUntil: "domcontentloaded", timeout: 3000 });
+    }
+
+    // manually added this type to the Page interface
+    // await page.waitForTimeout(3000)
+
+    try {
+
+      const chapterImageElement: ElementHandle | null = await page.$(
+        "img#img"
+      );
+
+      const chapterImageHandle: JSHandle<any> | undefined = await chapterImageElement?.getProperty("src")
+
+      const widthHandle: JSHandle<any> | undefined = await chapterImageElement?.getProperty("width")
+
+      const heightHandle: JSHandle<any> | undefined = await chapterImageElement?.getProperty("height")
+
+      const imageWidth = await widthHandle?.jsonValue();
+
+      const imageHeight = await heightHandle?.jsonValue();
+
+      const chapterImageUrl: any = await chapterImageHandle?.jsonValue()
+
+      // console.log("scrape of " + chapter + "success")
+      await browser.close();
+      res.send({ chapterImageUrl, imageWidth, imageHeight })
+    } catch (e) {
+      await page.reload({ waitUntil: "load", timeout: 3000 });
+      // console.log(e)
+      // console.log("page reloaded")
+
+      const chapterImageElement: ElementHandle | null = await page.$(
+        "img#img"
+      );
+
+      const chapterImageHandle: JSHandle<any> | undefined = await chapterImageElement?.getProperty("src")
+
+      const widthHandle: JSHandle<any> | undefined = await chapterImageElement?.getProperty("width")
+
+      const heightHandle: JSHandle<any> | undefined = await chapterImageElement?.getProperty("height")
+
+      const imageWidth = await widthHandle?.jsonValue();
+
+      const imageHeight = await heightHandle?.jsonValue();
+
+      const chapterImageUrl = await chapterImageHandle?.jsonValue()
+
+      // console.log("scrape of " + chapter + "success")
+      await browser.close();
+      res.send({ chapterImageUrl, imageWidth, imageHeight })
+    }
+
   }
 );
 
