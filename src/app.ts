66a03179aded659990636438afafd2a11209c3a4
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-10-28 01:57:28 -0500

    Forgot to include/require build route

--- src/app.ts
+++ src/app.ts
@@ -8,3 +8,4 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use(express.json());
 
 app.use("/api/manga", require("./routes/api/manga"));
+app.use("/api/build", require("./routes/api/build"));
