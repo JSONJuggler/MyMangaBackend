5e72c95c19968df382936bc27c2c742e78a10afa
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-11-03 07:25:52 -0600

    test

--- src/mangapanda/crawler/index.ts
+++ src/mangapanda/crawler/index.ts
@@ -29,7 +29,9 @@ export default class Crawler {
     });
 
     const page: Page = await browser.newPage();
-
+    page.on("console", (msg: ConsoleMessage): void =>
+      console.log("PAGE LOG:", msg.text())
+    );
     await page.setUserAgent(
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
     );
@@ -42,13 +44,39 @@ export default class Crawler {
   }
 
   async crawlInternal(page: Page, path: string, selectors: Array<Selector>) {
+    path === "testC" && await page.goto(path, { waitUntil: 'domcontentloaded' });
 
-    await page.goto(path, { waitUntil: 'domcontentloaded' });
-
+    console.log(path)
     if (selectors.length === 0) {
       return;
     }
 
+    switch (selectors[0].selector) {
+      case "select#pageMenu > option":
+      case "img#img":
+      case "ul.series_alpha":
+        let mangaLinks: Array<string> = await page.evaluate(sel => {
+          let returnedItems = [];
+          console.log(sel)
+          for (let ul of document.querySelectorAll(sel)) {
+            for (let anchor of ul.querySelectorAll("a")) {
+              let href = anchor.getAttribute("href");
+              console.log(href + "yu")
+              returnedItems.push(href);
+            }
+          }
+          return returnedItems;
+        }, selectors[0].selector);
+        console.log(mangaLinks.length)
+
+        for (let i = 0; i < mangaLinks.length; i++) {
+          await this.crawlInternal(page,
+            "", selectors[0].children)
+        }
+        return
+      default:
+        return
+    }
     if (selectors[0].selector === "img#img") {
       this.chpCounter++
       let imageLink: string = await page.evaluate(sel => {
@@ -59,7 +87,9 @@ export default class Crawler {
 
       this.result[this.chpCounter] = imageLink
 
-    } else {
+    }
+
+    if (selectors[0].selector === "select#pageMenu > option") {
       this.chpCounter = 0;
       let childrenLinks: Array<string> = await page.evaluate(sel => {
         let ret = [];
@@ -77,6 +107,8 @@ export default class Crawler {
           `${item}`, selectors[0].children)
       }
     }
+
+
   }
 
 }
