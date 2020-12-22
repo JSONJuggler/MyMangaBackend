0d765e71a97fb715d4a5fee8a9fa5ae64b7dec2f
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2020-12-22 16:16:55 -0600

    adding some global types

---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+export type SearchResult = {
+  searchResultCoverUrlString: string;
+  searchResultTitleString: string;
+  searchResultLinkString: string;
+  searchResultChapterCount: number;
+  searchResultTypeString: string;
+  mangaGenreString: string;
+};
+
+export type MangaChapter = {
+  mangaChapterTitleString: string;
+  mangaChapterLinkString: string;
+  mangaChapterNumber: number;
+  mangaChapterDateString: string;
+};
+
+export type MangaDetails = {
+  mangaTitleString: string;
+  mangaCoverUrlString: string;
+  mangaLinkString: string;
+  mangaAuthorString: string;
+  mangaArtistString: string;
+  mangaSummaryString: string;
+  mangaChapters: Array<MangaChapter>;
+};
+
+export type ChapterPage = {
+  chapterImageUrl: string;
+  imageWidth: string;
+  imageHeight: string;
+};
+
+export type ChapterPages = {
+  chapterPageUrls: Array<string>;
+  chapterImageUrls: Array<ChapterPage | undefined>;
+};
