cc1749ff9795f16b5a27ef0e460a5be34efcff70
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2020-12-21 21:16:24 -0600

    more remark configuration

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express, { Application } from 'express';
 import fs from 'fs';
 import remark from 'remark';
 import html from 'remark-html';
+import highlight from 'remark-highlight.js';
+import report from 'vfile-reporter';
+import lint from 'remark-preset-lint-recommended';
+import gfm from 'remark-gfm';
 
 const app: Application = express();
 const PORT = process.env.PORT || 5000;
@@ -17,9 +21,15 @@ app.get('/', (req, res) => {
     if (err) {
       console.log(err);
     }
-    const result = await remark().use(html).process(data);
-
-    res.send(result.toString());
+    remark()
+      .use(lint)
+      .use(highlight)
+      .use(gfm, { tableCellPadding: true, tablePipeAlign: false })
+      .use(html)
+      .process(data, (err, file) => {
+        console.error(report(err || file));
+        res.send(file.toString());
+      });
   });
 });
 
